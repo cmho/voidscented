1b89dd654a18dfc37fb5be42c86a5f0ff3b75b94
Carly Ho <cho@cliquestudios.com>
2016-04-12 10:02:56 -0500

    Change markov chain settings

--- application.rb
+++ application.rb
@@ -4,6 +4,6 @@ require 'markov_chains'
 
 get '/' do
 	data = open('https://dl.dropboxusercontent.com/u/2258962/voidscented.txt') { |io| io.read }
-	generator = MarkovChains::Generator.new(data, 3)
-	puts generator.get_sentences(5)
+	generator = MarkovChains::Generator.new(data, 2)
+	puts generator.get_sentences(3)
 end
