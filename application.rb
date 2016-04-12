12ce2cc81485dc7dfa542be2732460d557edf68b
Carly Ho <cho@cliquestudios.com>
2016-04-12 10:06:20 -0500

    Fix rendering

--- application.rb
+++ application.rb
@@ -5,5 +5,6 @@ require 'markov_chains'
 get '/' do
 	data = open('https://dl.dropboxusercontent.com/u/2258962/voidscented.txt') { |io| io.read }
 	generator = MarkovChains::Generator.new(data, 2)
-	puts generator.get_sentences(3)
+	text = generator.get_sentences(3).join("<br />")
+	erb text
 end
